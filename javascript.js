f8673478a005cb4c82208b0bf17b6738ca889e0f
Awa <ndeye-awa.ndiaye@msn.com>
2021-11-23 17:45:54 +0100

    displaytext

---
+++ javascript.js
@@ -0,0 +1,5 @@
+function displayPsondage() {
+    const showMessage = document.getElementById('psondage');
+    showMessage.innerHTML = 'Chaque trimestre, sera organisé un sondage pour évaluer votre niveau de morale. Vous serez amenés à nous donner vos ressentis et les axes d’amélioration de l’école W2G pour votre confort et votre bien être au sein du campus.';
+    showMessage.style.display = 'block';
+ }
